a3df90059317bd12d12f804a07a581a927d933f7
Zach <88857293+ZTefertiller@users.noreply.github.com>
2025-03-24 23:04:52 +0100

    Fix for prolificLinks.js & default for lab website.

--- prolific_consent/prolificLinks.js
+++ prolific_consent/prolificLinks.js
@@ -1,25 +1,24 @@
-let inactivityLink = 'https://app.prolific.com/submissions/complete?cc=C2K5DEKR';
-let abortStudyLink = 'https://app.prolific.com/submissions/complete?cc=C1J0QVQM';
-let noConsentLink = 'https://app.prolific.com/submissions/complete?cc=C1H9VW2B';
-let studyCompletedLink = 'https://app.prolific.com/submissions/complete?cc=C9FL91J6'
+let inactivityLink = 'https://franziskaknolle.com';
+let abortStudyLink = 'https://franziskaknolle.com';
+let noConsentLink = 'https://franziskaknolle.com';
+let studyCompletedLink = 'https://franziskaknolle.com'
+
+// if you use prolific etc, replace the links above with your prolific completion paths
 
 function abortStudy() {
-    if (confirm("Are you sure you want to abort the study? You will be redirected to Prolific and marked as incomplete.")) {
+    if (confirm("Are you sure you want to abort the study? You will be redirected and forfeit your task earnings.")) {
         studyInProgress = false;
         jatos.endStudyAndRedirect(abortStudyLink);
     }
 }
-
+window.studyCompletedLink = studyCompletedLink;
 window.noConsentLink = noConsentLink
+window.inactivityLink = inactivityLink
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Or if you already have the variable from prolificLinks.js:
-    const noConsentLink = window.noConsentLink; // if it's already defined
-
-    const anchor = document.getElementById("noConsentLink");
-    anchor.setAttribute("href", window.noConsentLink); 
-    // or simpler:
-    anchor.href = window.noConsentLink;
+    const noConsentAnchor = document.getElementById("noConsentLink");
+    if (noConsentAnchor) {
+        noConsentAnchor.href = window.noConsentLink;
+    }
 });
 
-window.inactivityLink = inactivityLink
